39027e10f42197eb02ce067ae88a4f05f5c5f35a
BenedictW <b.wilk80@wp.pl>
2021-02-04 01:01:55 +0100

    basic stuff

---
+++ hello.py
@@ -0,0 +1,10 @@
+# This program says hello and ask for my name
+print("Hello world!")
+print("What is Your name?")
+my_name=input()
+print("It's good to meet you "+my_name)
+print("The length of your name is:")
+print(len(my_name))
+print("What is your age?") #ask about age
+my_age=input()
+print("You will be "+str(int(my_age)+1)+ " in a year.")
